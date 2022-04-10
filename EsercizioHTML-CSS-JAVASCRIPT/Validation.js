0c227ec2c3a0255a985d83fdc6e5db9c0325c616
Pasquale Cannavacciuolo <48246188+Pasqualecannavacciuolo@users.noreply.github.com>
2022-04-10 14:11:25 +0200

    Improvements
    Resolved some bug on the validation

--- EsercizioHTML-CSS-JAVASCRIPT/Validation.js
+++ EsercizioHTML-CSS-JAVASCRIPT/Validation.js
@@ -1,49 +1,14 @@
 const validateForm = () => {
-    
-
     removeErrorMsg();
+    return checkValidity();
+}
 
-    // If all the checking go well redirect to my Github Page
-    if(validateCognome() && validateNome() && validateEmail() && formatEmail() && validateTelefono() && formatTelefono()) {
-        console.log("TUTTI TRUE");
+const checkValidity = () => {
+    validateTelefono()
+    if(!validateCognome() || !validateNome() || !validateTelefono() || !validateEmail()) {
+        return false;
+    } else {
         return true;
-    }
-
-    // If all the critical data fail the checkings
-    if(!validateCognome() && !validateNome() && !validateEmail() && !formatEmail() && !validateTelefono() && !formatTelefono()) {
-        console.log("TUTTI FALSI");
-        return false;
-    } else
-
-    // CHECKING FOR INDIVIDUAL ERRORS
-    if(validateCognome()==false) {
-        showErrorMsg('cognome-message', "Il cognome non può essere vuoto");
-        return false;
-    } else
-
-    if(validateNome()==false) {
-        showErrorMsg('nome-message', "Il nome non può essere vuoto");
-        return false;
-    } else
-
-    if (validateEmail() == false) {
-        showErrorMsg('email-message', "L'email non può essere vuota");
-        return false;
-    } else
-
-    if(formatEmail()==false) {
-        showErrorMsg('email-message', "Non hai inserito una mail valida");
-        return false;
-    } else
-
-    if(validateTelefono()==false) {
-        showErrorMsg('telefono-message', "Il telefono non può essere vuoto");
-        return false;
-    } else 
-
-    if(formatTelefono()==false) {
-        showErrorMsg('telefono-message', "Non hai inserito un numero valido");
-        return false;
     }
 }
 
@@ -74,50 +39,36 @@ const validateNome = () => {
 
 // Checkign if email is empty
 const validateEmail = () => {
+    var emailf = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     let email = document.forms["form-dati"]["email"].value;
     if (email == '') {
         showErrorMsg('email-message', "L'email non può essere vuota");
         return false;
-    } else {
-        return true;
+    } else if(email != '') {
+        if(!emailf.test(email)) {
+            showErrorMsg('email-message', "Non hai inserito una email valida");
+            return false;
+        }
     }
-}
-
-
-// Checking if the email is the correct format
-const formatEmail = () => {
-    var emailf = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let email = document.forms["form-dati"]["email"].value;
-    if(!emailf.test(email)) {
-        showErrorMsg('email-message', "Non hai inserito una email valida");
-        return false;
-    } else {
-        return true;
-    }
+    return true;
 }
 
 
 // Checkign if telefono is empty
 const validateTelefono = () => {
+    var phoneno = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
     let telefono = document.forms["form-dati"]["telefono"].value;
+
     if (telefono == '') {
         showErrorMsg('telefono-message', "Il telefono non può essere vuoto");
         return false;
-    } else {
-        return true;
+    } else if (telefono != '') {
+        if (!phoneno.test(telefono)) {
+            showErrorMsg('telefono-message', "Non hai inserito un numero valido");
+            return false;
+        }
     }
-}
-
-// Checking if telefono is the correct format
-const formatTelefono = () => {
-    var phoneno = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-    let telefono = document.forms["form-dati"]["telefono"].value;
-    if(!phoneno.test(telefono)) {
-        showErrorMsg('telefono-message', "Non hai inserito un numero valido");
-        return false;
-    } else {
-        return true;
-    }
+    return true;
 }
 
 
@@ -133,17 +84,22 @@ const showErrorMsg = (e_id, msg) => {
 const removeErrorMsg = () => {
     let cognome = document.getElementById('cognome-message');
     let nome = document.getElementById('nome-message');
+    let email = document.getElementById('email-message');
     let telefono = document.getElementById('telefono-message');
 
-    if(cognome.innerHTML != "") {
+    if(validateCognome()) {
         cognome.innerHTML = "";
-    } else
+    }
 
-    if(nome.innerHTML != "") {
+    if(validateNome()) {
         nome.innerHTML = "";
-    } else
+    }
 
-    if(telefono.innerHTML != "") {
+    if(validateEmail()) {
+        email.innerHTML = "";
+    }
+
+    if(validateTelefono()) {
         telefono.innerHTML = "";
     }
 }
